2a761a6510e29faf82f565ea1cdf840ae0dec57f
Shashi Kant <112147955+ShashiKant17092003@users.noreply.github.com>
2025-06-28 12:53:21 +0530

    Update Home.jsx
    Added tryhackme

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,24 +5,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { useEffect, useState } from 'react';
-import {  githubStats } from '../utils/fetchGitHubStats';
+import { githubStats } from '../utils/fetchGitHubStats';
 import { NavLink } from 'react-router-dom';
-
-
 
 const Home = () => {
   const [stats, setStats] = useState({ projects: 0, languagesUsed: 0 });
 
   useEffect(() => {
-    githubStats().then(data => setStats({projects:data.repos,languagesUsed:data.langs}));
+    githubStats()
+      .then(data => setStats({ projects: data.repos, languagesUsed: data.langs }))
+      .catch(() => setStats({ projects: 0, languagesUsed: 0 })); // fallback
   }, []);
 
   return (
     <div className='container'>
-     <div className="header">
+      <div className="header">
         <div className="text-area">
           <div className="title">Software Developer</div>
-          {/* eslint-disable-next-line react/no-unescaped-entities */}
           <div className="name white">Hello I'm</div>
           <div className="name green">Shashi Kant</div>
           <div className="description">
@@ -33,28 +32,31 @@ const Home = () => {
           <div className="buttons">
             <div className="button btn">
               <a href="https://drive.google.com/file/d/14vuZSDviGcxV7B9nE2fYBpN2e8WkbE1n/view?usp=drive_link" target='_blank' rel="noopener noreferrer">
-                DOWNLOAD CV <FontAwesomeIcon icon={faDownload} className='download'/>
+                DOWNLOAD CV <FontAwesomeIcon icon={faDownload} className='download' />
               </a>
             </div>
             <div className="button social">
               <div className="socialIcons">
-                <NavLink to="/github"><FontAwesomeIcon icon={faGithub}  /> </NavLink>
+                <NavLink to="/github"><FontAwesomeIcon icon={faGithub} /></NavLink>
               </div>
               <div className="socialIcons">
-                <a href="https://www.linkedin.com/in/shashi-kant-068587250/" target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin}  /></a>
+                <a href="https://www.linkedin.com/in/shashi-kant-068587250/" target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
               </div>
               <div className="socialIcons">
-                <a href="https://www.instagram.com/shashikant_1793_/?igsh=bmtoMGRmdzAxYzdo" target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram}  /></a>
+                <a href="https://www.instagram.com/shashikant_1793_/?igsh=bmtoMGRmdzAxYzdo" target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
               </div>
               <div className="socialIcons">
-                <a href="#" target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter}  /></a>
+                <a href="#" target='_blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
               </div>
             </div>
           </div>
         </div>
-        <div className="img"><img src={Sk} alt="Profile" /></div>
-     </div>
-     <div className="footer">
+        <div className="img">
+          <img src={Sk} alt="Shashi Kant" />
+        </div>
+      </div>
+
+      <div className="footer" style={{marginTop:'-15px'}}>
         <div className="box">
           <div className="inbox box1">
             <div className="text-box num">00</div>
@@ -71,6 +73,7 @@ const Home = () => {
             </div>
           </div>
         </div>
+
         <div className="box">
           <div className="inbox">
             <div className="text-box num">{String(stats.languagesUsed).padStart(2, '0')}</div>
@@ -79,17 +82,18 @@ const Home = () => {
               <div className="text-desc">Mastered</div>
             </div>
           </div>
-          {/* <div className="inbox">
-            <div className="text-box num">{String(stats.totalCommits/10).padStart(2, '0')}</div>
-            <div className="text-box">
-              <div className="text-desc">Code</div>
-              <div className="text-desc">Commits</div>
-            </div>
-          </div> */}
+          <div className="inbox">
+            <iframe
+              src="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=4929208"
+              style={{ border: 'none', width: '100%', height: '140px' , paddingTop:'30px'}}
+              title="TryHackMe Badge"
+              scrolling="no"
+            />
+          </div>
         </div>
-     </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
